4eb1502ec273590ebc50cb6c8fc902a313b8c53f
Bart Schuurmans <bart@minnozz.com>
2022-12-10 14:45:34 +0100

    Day 1, part 2

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 
 fn main() {
     let lines = include_str!("input.txt").lines().collect_vec();
-    let max_total = lines
+    let elf_calories = lines
         .split(|line| line.is_empty())
         .map(|lines| {
             lines
@@ -10,6 +10,14 @@ fn main() {
                 .filter_map(|s| s.parse::<u32>().ok())
                 .sum::<u32>()
         })
-        .max();
-    println!("{:?}", max_total);
+        .sorted()
+        .collect_vec();
+
+    // Part 1
+    let max_total = elf_calories.last().unwrap();
+    println!("The Elf carrying the most calories carries {}", max_total);
+
+    // Part 2
+    let top_3_total = elf_calories.iter().rev().take(3).sum::<u32>();
+    println!("The top 3 Elves are carrying {} calories", top_3_total);
 }
