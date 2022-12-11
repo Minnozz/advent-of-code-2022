b2e64d1fd2323dde06b96fefa3d21eb6d983d81b
Bart Schuurmans <bart@minnozz.com>
2022-12-11 20:19:33 +0100

    Day 2, part 1, version 3

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,53 +1,10 @@
-#[derive(PartialEq)]
-enum Move {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3,
-}
+// A, X, 0: Rock (1 pt)
+// B, Y, 1: Paper (2 pt)
+// C, Z, 2: Scissors (3 pt)
 
-impl From<&str> for Move {
-    fn from(str: &str) -> Self {
-        match str {
-            "A" | "X" => Self::Rock,
-            "B" | "Y" => Self::Paper,
-            "C" | "Z" => Self::Scissors,
-            _ => panic!("invalid move: {}", str),
-        }
-    }
-}
-enum Outcome {
-    Loss = 0,
-    Draw = 3,
-    Win = 6,
-}
-
-impl From<&str> for Outcome {
-    fn from(str: &str) -> Self {
-        match str {
-            "X" => Self::Loss,
-            "Y" => Self::Draw,
-            "Z" => Self::Win,
-            _ => panic!("invalid outcome: {}", str),
-        }
-    }
-}
-
-fn battle(their: &Move, our: &Move) -> Outcome {
-    if their == our {
-        return Outcome::Draw;
-    }
-    match (their, our) {
-        (Move::Rock, Move::Scissors)
-        | (Move::Paper, Move::Rock)
-        | (Move::Scissors, Move::Paper) => Outcome::Loss,
-        (Move::Rock, Move::Rock)
-        | (Move::Paper, Move::Paper)
-        | (Move::Scissors, Move::Scissors) => Outcome::Draw,
-        (Move::Rock, Move::Paper)
-        | (Move::Paper, Move::Scissors)
-        | (Move::Scissors, Move::Rock) => Outcome::Win,
-    }
-}
+// X, 0: Loss (0 pt)
+// Y, 1: Draw (3 pt)
+// Z, 2: Win (6 pt)
 
 fn main() {
     let lines = include_str!("input.txt").lines();
@@ -55,12 +12,17 @@ fn main() {
     // Part 1
     let total_score = lines
         .map(|line| {
-            let (theirs, ours) = line.split_once(" ").unwrap();
-            let their_move: Move = theirs.into();
-            let our_move: Move = ours.into();
-            let outcome = battle(&their_move, &our_move);
-            our_move as u32 + outcome as u32
+            let chars = line.chars().collect::<Vec<_>>();
+            let their_move = chars[0] as i32 - 'A' as i32;
+            let our_move = chars[2] as i32 - 'X' as i32;
+            let outcome = match (their_move - our_move + 3) % 3 {
+                0 => 3, // Draw
+                1 => 0, // Loss
+                2 => 6, // Win
+                _ => panic!("invalid line: {}", line),
+            };
+            our_move + 1 + outcome
         })
-        .sum::<u32>();
+        .sum::<i32>();
     println!("Total score: {}", total_score);
 }
