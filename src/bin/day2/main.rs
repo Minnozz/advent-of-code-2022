66702ceac19d12730568566897d9b8021fa724ed
Bart Schuurmans <bart@minnozz.com>
2022-12-11 20:04:49 +0100

    Day 2, part 1, version 2

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,29 +1,66 @@
-const LOSS: u32 = 0;
-const DRAW: u32 = 3;
-const WIN: u32 = 6;
+#[derive(PartialEq)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl From<&str> for Move {
+    fn from(str: &str) -> Self {
+        match str {
+            "A" | "X" => Self::Rock,
+            "B" | "Y" => Self::Paper,
+            "C" | "Z" => Self::Scissors,
+            _ => panic!("invalid move: {}", str),
+        }
+    }
+}
+enum Outcome {
+    Loss = 0,
+    Draw = 3,
+    Win = 6,
+}
+
+impl From<&str> for Outcome {
+    fn from(str: &str) -> Self {
+        match str {
+            "X" => Self::Loss,
+            "Y" => Self::Draw,
+            "Z" => Self::Win,
+            _ => panic!("invalid outcome: {}", str),
+        }
+    }
+}
+
+fn battle(their: &Move, our: &Move) -> Outcome {
+    if their == our {
+        return Outcome::Draw;
+    }
+    match (their, our) {
+        (Move::Rock, Move::Scissors)
+        | (Move::Paper, Move::Rock)
+        | (Move::Scissors, Move::Paper) => Outcome::Loss,
+        (Move::Rock, Move::Rock)
+        | (Move::Paper, Move::Paper)
+        | (Move::Scissors, Move::Scissors) => Outcome::Draw,
+        (Move::Rock, Move::Paper)
+        | (Move::Paper, Move::Scissors)
+        | (Move::Scissors, Move::Rock) => Outcome::Win,
+    }
+}
 
 fn main() {
     let lines = include_str!("input.txt").lines();
 
+    // Part 1
     let total_score = lines
         .map(|line| {
             let (theirs, ours) = line.split_once(" ").unwrap();
-            let move_score = match ours {
-                "X" => 1,
-                "Y" => 2,
-                "Z" => 3,
-                _ => panic!("Invalid move {} in line {}", ours, line),
-            };
-            let win_score = match (theirs, ours) {
-                ("A", "Z") | ("B", "X") | ("C", "Y") => LOSS,
-                ("A", "X") | ("B", "Y") | ("C", "Z") => DRAW,
-                ("A", "Y") | ("B", "Z") | ("C", "X") => WIN,
-                _ => panic!("Invalid line: {}", line),
-            };
-            move_score + win_score
+            let their_move: Move = theirs.into();
+            let our_move: Move = ours.into();
+            let outcome = battle(&their_move, &our_move);
+            our_move as u32 + outcome as u32
         })
         .sum::<u32>();
-
-    // Part 1
     println!("Total score: {}", total_score);
 }
